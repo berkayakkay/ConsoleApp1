6929b6df8e0168a022105bd18d441521da69154a
berkay <berkay@DESKTOP-0DUNFO6>
2021-02-01 15:39:28 +0300

    4.ders

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "A", "B", "C", "D" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler = new string[5];
+            isimler[4] = "Z";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+        }
+
+        
+    }
+}
+
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {//encapsulation
+        public void Add(Product product1,Product product2)
+        {
+            Console.WriteLine(product1.ProductName + " eklendi.");
+            Console.WriteLine(product2.ProductName + " eklendi.");
+
+        }
+
+
+    }
+
+       
+}
